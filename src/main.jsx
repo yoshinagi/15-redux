b26019973339ca5d6a89439940377b4dc905dd8a
Yoshi Nagi <nagiyoshi13@gmail.com>
2024-02-12 10:53:57 +0900

    add account and customer logics

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+
+import store from './store';
+
+store.dispatch({ type: 'account/deposit', payload: 250 });
+console.log(store.getState());
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
